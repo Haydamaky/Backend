c05244387bb773e94b1afe1bc6cdb150758d943d
Taras Yurchenko <turchenko730@gmail.com>
2024-10-07 15:59:44 +0300

    feat: created jwt strategies

---
+++ src/auth/strategy/jwt.rt.strategy.ts
@@ -0,0 +1,34 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { JwtPayload } from '../types/jwtPayloadType.type';
+import { Request } from 'express';
+import { JwtPayloadWithRt } from '../types/jwtPayloadWithRt.type';
+
+@Injectable()
+export class JwtRtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          const token = request.cookies['refresh_token'];
+          return token;
+        },
+      ]),
+      secretOrKey: config.get('REFRESH_TOKEN_PUB_KEY'),
+      algorithms: ['RS256'],
+      passReqToCallback: true,
+    });
+  }
+  validate(req: Request, payload: JwtPayload): JwtPayloadWithRt {
+    const refreshToken = req.cookies['refresh_token'];
+
+    if (!refreshToken) throw new ForbiddenException('Refresh token malformed');
+
+    return {
+      ...payload,
+      refreshToken,
+    };
+  }
+}
