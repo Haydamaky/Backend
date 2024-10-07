eb6a69e840bef46c2059bf1cea796183aeb40345
Taras Yurchenko <turchenko730@gmail.com>
2024-10-07 15:58:07 +0300

    feat: created auth dtos

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,25 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class SignUpDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  nickname: string;
+}
+
+export class SignInDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
