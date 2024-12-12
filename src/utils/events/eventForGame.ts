c8c0d6342d2d100cd19b69d013c1322ffb918d48
Anton <ponochovnyi.anton@LLL.kpi.ua>
2024-12-12 16:40:00 +0200

    feat: added events file

---
+++ src/utils/events/eventForGame.ts
@@ -0,0 +1,18 @@
+export const gameEvents = {
+  JOIN_GAME:'joinGame',
+  ROLL_DICE:'rollDice',
+  BUY_FIELD:'buyField',
+  RAISE_PRISE:'raisePrise',
+  FIELD_PAYMENT:'fieldPayment',
+  LEAVE_GAME:'leaveGame',
+
+  START_GAME:'startGame',
+  MAKE_TURN:'makeTurn',
+  END_TURN:'endTurn',
+
+  START_AUCTION:'startAuction',
+  UPDATE_AUCTION:'updateAuction',
+  END_AUCTION:'endAuction',
+
+  UPDATE_GAME:'updateGame',
+}
