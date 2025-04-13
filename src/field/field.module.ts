3fb8f51b40df632c7ec745334b044bfbfb1bb932
Taras Yurchenko <turchenko730@gmail.com>
2025-04-13 21:12:17 +0300

    feat: added FieldValidator class

--- src/field/field.module.ts
+++ src/field/field.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { FieldService } from './field.service';
 
 @Module({
-  providers: [FieldService]
+  providers: [FieldService],
+  exports: [FieldService],
 })
 export class FieldModule {}
