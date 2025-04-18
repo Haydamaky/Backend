5115e4152b9c70447a5a83b6ccee3d2cf4e123e2
Taras Yurchenko <turchenko730@gmail.com>
2025-04-18 14:22:41 +0300

    feat: created one player handler

--- src/game/handlers/onePlayerInvolved.handler.ts
+++ src/game/handlers/onePlayerInvolved.handler.ts
@@ -1,11 +1,17 @@
 import { SecretAnalyzer } from 'src/secret/secretAnalyzer';
 import { BaseHandler } from '../../common/base.handler';
-export class PassTurnHandler extends BaseHandler<SecretAnalyzer> {
+import { WsException } from '@nestjs/websockets';
+export class onePlayerInvolvedHandler extends BaseHandler<SecretAnalyzer> {
+  throw() {
+    throw new WsException(
+      'You cant pay to bank because one user and he doesnt have to pay'
+    );
+  }
   canHandle() {
     return (
-      this.analyzer.isOwnedByCurrentUser() ||
-      (this.analyzer.isNotOwned() && !this.analyzer.isAffordableForSomeone()) ||
-      this.analyzer.isSkipable()
+      (this.analyzer.inOnePlayerInvolved() &&
+        this.analyzer.secretInfo.amounts[0] > 0) ||
+      this.throw()
     );
   }
 }
