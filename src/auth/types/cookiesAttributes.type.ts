875e5cc716631737af69ad256f72dfe37e63648d
Illia Kroshka <illya182004@gmail.com>
2025-06-06 18:20:35 +0300

    fixed cookies attributes

--- src/auth/types/cookiesAttributes.type.ts
+++ src/auth/types/cookiesAttributes.type.ts
@@ -3,11 +3,14 @@ export type AccessCookieAttributes = {
   sameSite: 'lax' | 'strict' | 'none';
   secure?: boolean;
   domain: string;
+  path: string;
+  maxAge: number;
 };
 
 export type RefreshCookieAttributes = {
   httpOnly: boolean;
   secure?: boolean;
+  maxAge: number;
   sameSite: 'lax' | 'strict' | 'none';
   path: string;
   domain: string;
