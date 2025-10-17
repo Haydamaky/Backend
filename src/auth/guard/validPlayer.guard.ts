93d8f763713dfea4d792afd82c46eebaec6f1239
Taras Yurchenko <turchenko730@gmail.com>
2025-10-17 16:44:48 +0300

    feat: added right exceptions in guards

--- src/auth/guard/validPlayer.guard.ts
+++ src/auth/guard/validPlayer.guard.ts
@@ -15,10 +15,18 @@ export class ValidPlayerGuard implements CanActivate {
 
   async canActivate(context: any): Promise<boolean | any> {
     const client = context.switchToWs().getClient();
+    const data = context.switchToWs().getData();
     const userId = client.data.jwtPayload.sub;
     if (!userId) {
       if (!client.handshake.headers.cookie) {
-        throw new WsException('Unauthorized');
+        throw new WsException({
+          code: 'USER_NOT_AUTHENTICATED',
+          message: 'You must log in before performing this action',
+          details: {
+            action: 'ValidPlayerGuard',
+          },
+          requestId: data.requestId,
+        });
       }
       const { accessToken } = parse(client.handshake.headers.cookie);
       try {
@@ -27,7 +35,14 @@ export class ValidPlayerGuard implements CanActivate {
         });
         client.data.jwtPayload = decoded;
       } catch (ex) {
-        throw new WsException(ex.message);
+        throw new WsException({
+          code: 'USER_NOT_AUTHENTICATED',
+          message: ex.message,
+          details: {
+            action: 'ValidPlayerGuard',
+          },
+          requestId: data.requestId,
+        });
       }
     }
     let gameId = client.data.gameId;
@@ -41,6 +56,7 @@ export class ValidPlayerGuard implements CanActivate {
         details: {
           action: 'ValidPlayerGuard',
         },
+        requestId: data.requestId,
       });
     try {
       const player = await this.playerService.findByUserAndGameId(
@@ -48,13 +64,35 @@ export class ValidPlayerGuard implements CanActivate {
         gameId
       );
       if (player?.game?.status !== 'ACTIVE')
-        throw new WsException('Game is not active anymore');
-      if (player?.lost) throw new WsException('You have already lost');
+        throw new WsException({
+          code: 'GAME_IS_NOT_ACTIVE',
+          message: 'Game is not active anymore',
+          details: {
+            action: 'ValidPlayerGuard',
+          },
+          requestId: data.requestId,
+        });
+      if (player?.lost)
+        throw new WsException({
+          code: 'HAVE_ALREADY_LOST',
+          message: 'You have already lost',
+          details: {
+            action: 'ValidPlayerGuard',
+          },
+          requestId: data.requestId,
+        });
       client.player = player;
       client.game = player.game;
       return true;
     } catch (ex) {
-      throw new WsException(ex.message);
+      throw new WsException({
+        code: 'UNHANDLED',
+        message: 'Error occured with db',
+        details: {
+          action: 'ValidPlayerGuard',
+        },
+        requestId: data.requestId,
+      });
     }
   }
 }
